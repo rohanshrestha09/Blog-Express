bfc73393b16b7be568f97dde0deff1ea270ab71a
rohanshrestha09 <shresthacr11@gmail.com>
2022-09-02 17:02:23 +0545

    Auth bypass for single blog fetching

--- dist/routes/blog.js
+++ dist/routes/blog.js
@@ -10,7 +10,7 @@ const blogValidator = require('../middleware/blogValidator');
 const router = express_1.default.Router();
 router.get('/blog', getAllBlogs);
 router.get('/blog/categorised', getCategorisedBlog);
-router.get('/blog/:_blogId', getBlog);
+router.get('/blog/:_blogId', blogValidator, getBlog);
 router.get('/blog/genre', getGenre);
 router.use(['/blog/:_blogId', '/blog/:_blogId/publish'], auth, blogValidator);
 router.post('/blog', auth, postBlog);
