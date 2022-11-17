8119c3da9d27cc103d0b0be30ce816a80d9dcff1
rohanshrestha09 <shresthacr11@gmail.com>
2022-11-17 18:06:18 +0545

    Ready for testing

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express, { Application } from 'express';
-import { initializeApp, cert } from 'firebase-admin/app';
+import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
 import fileUpload from 'express-fileupload';
 import connectDB from './db';
 const rateLimit = require('express-rate-limit');
@@ -39,7 +39,18 @@ const limiter = rateLimit({
 const serviceAccount = require('./blog-sansar-firebase-adminsdk-8snwe-96b9089a8c');
 
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    type: 'service_account',
+    project_id: 'blog-sansar',
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  } as ServiceAccount),
   storageBucket: 'gs://blog-sansar.appspot.com',
 });
 
